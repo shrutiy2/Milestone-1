642124f00b3ead63adeaedccdd7aa28df1d9c1ef
sjydl0 <100722904+sjydl0@users.noreply.github.com>
2022-03-04 18:50:28 -0600

    Update gc_content(dna_list).py

--- gc_content(dna_list).py
+++ gc_content(dna_list).py
@@ -1,7 +1,5 @@
 def gc_content(dna_list):
     gc_dict = {}
-    for i in range(len(dna_list)):
+    for i in range(1,len(dna_list)):
         gc_dict[i] = dna_list[i].count('G') + dna_list[i].count('C')
-    return (max(gc_dict, key=gc_dict.get),((max(gc_dict.values()))/(len(dna_list[max(gc_dict, key=gc_dict.get)])))*100)
-    
-    
+    return (max(gc_dict, key=gc_dict.get) ,(((max(gc_dict.values()))/(len(dna_list[max(gc_dict, key=gc_dict.get)])))*100))
